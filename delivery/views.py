332d4964c5582f3900deed87fedf47116297b876
TallleP <csgoprotop1@gmail.com>
2026-05-06 00:04:38 +0300

    Lab 5

--- delivery/views.py
+++ delivery/views.py
@@ -1,33 +1,34 @@
-
-
+from django.shortcuts import render
+from .models import Category, Dish
 
 def home_view(request):
     context = {
-        'title': 'Головна: Доставка їжі',
-        'content': 'Вітаємо на сайті! Тут ви можете обрати найсмачнішу їжу.',
+        'title': 'Головна сторінка',
+        'categories': Category.objects.all(),
+        'dishes': Dish.objects.all(),
         'is_home': True
     }
     return render(request, 'delivery/page.html', context)
 
-
 def about_view(request):
     context = {
         'title': 'Про нас',
-        'content': 'Ми доставляємо їжу за 30 хвилин або безкоштовно!',
+        'categories': Category.objects.all(),
+        # ОНОВЛЕНИЙ ТЕКСТ БЕЗ ЛАБИ
+        'content': '''
+        Ми — "Доставка від Паші", найшвидший сервіс у місті Луцьк! 
+        Готуємо з любов'ю та доставляємо гарячим прямісінько до ваших дверей. 
+        Наші кур'єри знають кожну вулицю міста, тому ваша піца ніколи не охолоне. 
+        ''',
         'is_home': False
     }
     return render(request, 'delivery/page.html', context)
-
 
 def contacts_view(request):
     context = {
         'title': 'Наші контакти',
-        'content': 'Телефон: +38 (099) 123-45-67. Чекаємо на дзвінок!',
+        'categories': Category.objects.all(),
+        'content': 'Телефонуйте для замовлення: +38 (099) 123-45-67',
         'is_home': False
     }
     return render(request, 'delivery/page.html', context)
-
-
-from django.shortcuts import render
-
-# Create your views here.
