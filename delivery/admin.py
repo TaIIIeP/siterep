5b9e155916ada8fe3e2657089999771b810b52c8
TallleP <csgoprotop1@gmail.com>
2026-05-05 23:47:25 +0300

    Lab 4: Created models, connected tables and configured admin panel

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category, Dish, Courier
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'created_at', 'updated_at')
+
+
+@admin.register(Courier)
+class CourierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'created_at', 'updated_at')
+
+
+from django.contrib import admin
 
 # Register your models here.
--- delivery/models.py
+++ delivery/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва категорії")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категорія"
+        verbose_name_plural = "Категорії"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Назва страви")
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Ціна")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категорія")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Страва"
+        verbose_name_plural = "Страви"
+
+
+class Courier(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Ім'я кур'єра")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Кур'єр"
+        verbose_name_plural = "Кур'єри"
+
+
+from django.db import models
+
 # Create your models here.
